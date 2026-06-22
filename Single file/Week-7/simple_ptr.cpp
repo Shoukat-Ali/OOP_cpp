e794a0789027ec6d37bfa47bb4a0b3e303f1d8bb
Shoukat Ali <shoukat.1983@gmail.com>
2026-06-22 08:35:59 +0500

    A simple example to demonstrate basic use of pointer

---
+++ Single file/Week-7/simple_ptr.cpp
@@ -0,0 +1,37 @@
+/**
+ * This is an attempt to demonstrate the use of pointer variable.
+ * 
+ * To compile and create and executable file of this program, , please run the following GNU C++ command in a terminal:
+ * 
+ *      g++ -Wall -Werror -std=c++11 simple_ptr.cpp -o test
+ */
+
+#include <iostream>
+
+int main() {
+    using std::cout;
+    using std::endl;
+
+    int num{7};
+    int* nPtr{nullptr};
+
+    cout << "The address of num is:   " << &num << endl;
+    cout << "The address of nPtr is:  " << &nPtr << endl;
+    
+    nPtr = &num;
+    cout << "After nPtr = &num,  we have" << endl;
+    cout << "The content of nPtr is:  " << nPtr << endl;
+    if (nPtr) {
+    // if (nPtr != nullptr) {
+        cout << "The content of *nPtr is: " << *nPtr << endl;
+    }
+
+    *nPtr = 11;
+    cout << "After *nPtr = 11, we have" << endl;
+    cout << "The content of num is:   " << num << endl;
+    if (nPtr != nullptr) {
+        cout << "The content of *nPtr is: " << *nPtr << endl;
+    }
+
+    return 0;
+}
